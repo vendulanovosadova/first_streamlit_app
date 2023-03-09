5454f1571d1f55e836eaac0394197edd38f32fb5
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-09 12:28:27 +0000

    Update streamlit_app.py
    added fruits_to_show = my_fruit_list.loc[fruits_selected]
    so when the fruit is picked, the table updates accordingly

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include, they choose a fruit by a number, or you can manually code the fruit you like to select 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
