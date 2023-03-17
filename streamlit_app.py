30bab73175295e851077860de196060c3f81ce15
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-17 20:09:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,3 @@ streamlit.dataframe(my_data_rows)
 
  
 
- #New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
