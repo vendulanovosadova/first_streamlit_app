989ddf7c0ae00cbb584ca9bf4dad36d006fb88c1
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-20 22:03:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,21 +19,20 @@ streamlit.header('🍌🥑 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include, they choose a fruit by a number, or you can manually code the fruit you like to select 
+##### Let's put a pick list here, they can pick the fruit they want to include, they choose a fruit by a number, or you can manually code the fruit you like to select 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-#Create the repeatable code block (called a function)
+#New Section to display fruityvice api response Create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice): 
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       return fruityvice_normalized
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()
+    return fruityvice_normalized
    
-##New section to display fruityvice api response
+#New section to display fruityvice api response <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -68,7 +67,7 @@ if streamlit.button('Get Fruit List'):
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('from streamlit')")  #<<<<< I need to change this part in brackets, so the value is not hard-coded. 
+         my_cur.execute("insert into fruit_load_list values (' " + my_fruit_list.index +" ')")  #<<<<<(my_fruit_list.index) I need to change this part in brackets, so the value is not hard-coded. 
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
