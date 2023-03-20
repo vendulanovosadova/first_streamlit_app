81da2d0831566af354e77bc26a5a7d8ca7a6a03b
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-20 23:21:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
-import streamlit 
+import streamlit
+import pandas
+import requests
+import snowflake.connector 
+from urllib.error import URLError 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
@@ -8,7 +12,7 @@ streamlit.text('🥑🍞🐔 Avocado Toast')
 streamlit.header('🍌🥑 Build Your Own Fruit Smoothie 🥝🍇')
 
 # let's add CSV file with fruit info ---- CSV table part
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +30,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)       #<<<< here we set up the end user enters the fruit manually, connects with streamlit input from the row above
 
 
-import requests                                                                         #<< as per above selection, it returns the info from Fruitivice website. 
+#import requests                                                                         #<< as per above selection, it returns the info from Fruitivice website. 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  #<< as per above it returns the info from Fruitivice website. 
 ##streamlit.text(fruityvice_response.json()) 
 
