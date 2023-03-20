dfa4a618e7bff1ae56acd926e4e4ef97b544666d
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-20 22:54:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,28 +7,28 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞🐔 Avocado Toast')
 streamlit.header('🍌🥑 Build Your Own Fruit Smoothie 🥝🍇')
 
-# let's add CSV file with fruit info)
+# let's add CSV file with fruit info ---- CSV table part
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include, they choose a fruit by a number, or you can manually code the fruit you like to select 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]                        #<<<< as per your selection, it will return info from CSV file with nutrition info
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show) #<<< remove this 
 
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice) #<<<< here we set up the end user enters the fruit manually
+streamlit.write('The user entered ', fruit_choice) #<<<< here we set up the end user enters the fruit manually, connects with streamlit input from the row above
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-##streamlit.text(fruityvice_response.json()) #just writes data on the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #<< as per above selection, it returns the info from Fruitivice website. 
+##streamlit.text(fruityvice_response.json()) 
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -38,8 +38,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()  #<<< selects all rows from the table from Snowflake, still adds a manual input from the Streamlit app. 
+my_cur.execute("select * from fruit_load_list") #<<< this is a Snowflake table, into which people also add manually via Streamlit app
+my_data_rows = my_cur.fetchall()                #<<< selects all rows from the table from Snowflake, still adds a manual input from the Streamlit app. 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
@@ -47,7 +47,8 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit) #<<<< here we set up the end user enters the fruit manually
 
-
+#This will not work correctly, but just go with it for now
+my_cur_execute("insert into fruit_load_list values ('from Streamlit')")
 
 
 
