e816b6a9a1ba8bdd515bdd8a81a0d86831c45d54
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-20 23:08:34 +0000

    Update streamlit_app.py
    Lesson 12 - Time to Tidy up?

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,29 +17,29 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]                        #<<<< as per your selection, it will return info from CSV file with nutrition info
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show) #<<< this whows the CSV breakdown below the table
+streamlit.dataframe(fruits_to_show) #<<< this shows the CSV breakdown below the table
 
-
-#New section to display fruityvice api response
+----------------------------------------------------------------------------------------------PART 3--------------------------------------------------
+#New section to display fruityvice api response - FIRST ROW WITH MANUAL INPUT
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice) #<<<< here we set up the end user enters the fruit manually, connects with streamlit input from the row above
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')       #<<< ROW WITH MANUAL INPUT, we call in fruit_choice row
+streamlit.write('The user entered ', fruit_choice)       #<<<< here we set up the end user enters the fruit manually, connects with streamlit input from the row above
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #<< as per above selection, it returns the info from Fruitivice website. 
+import requests                                                                         #<< as per above selection, it returns the info from Fruitivice website. 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  #<< as per above it returns the info from Fruitivice website. 
 ##streamlit.text(fruityvice_response.json()) 
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment -what does the next line do?                                   #<<<<< ???
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())               #<<<<< ???
+# write your own comment - what does this do?                                           #<<<<< ???    
 
 
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+import snowflake.connector                                                 # imports Snowflake table "fruit_load_list"
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])     #<<< references setting in Streamlit app, where we put password to connect with Snowflake
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list") #<<< this is a Snowflake table, into which people also add manually via Streamlit app
-my_data_rows = my_cur.fetchall()                #<<< selects all rows from the table from Snowflake, still adds a manual input from the Streamlit app. 
+my_cur.execute("select * from fruit_load_list")           #<<< this is a Snowflake table, into which people also add manually via Streamlit app
+my_data_rows = my_cur.fetchall()                          #<<< selects all rows from the table from Snowflake, still adds a manual input from the Streamlit app. 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
