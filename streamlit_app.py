37c3bd7afd16fdf7e8902583c83c36e977e0d0b8
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-20 22:25:40 +0000

    Update streamlit_app.py
    Lesson 12 - Import Snowflake Package into Your App - import Snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
+import snowflake.connectot
 streamlit.dataframe(fruityvice_normalized)
