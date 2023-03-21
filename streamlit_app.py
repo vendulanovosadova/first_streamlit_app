f8a6308057bae60721f454767e3a134c572f727f
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-21 17:58:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,25 +57,34 @@ if streamlit.button('Get Fruit Load List'):                                    #
       my_data_rows = get_fruit_load_list()                       
       streamlit.dataframe(my_data_rows)   
         
-       
+#Allow the end user to add fruit to the list manually -------
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         return "Thanks for adding "+new_fruit
+                              
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])     
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)       
            
+        
+        
+        
+        
 
-#don't run anything past this while we troubleshoot
+#don't run anything past this while we troubleshoot-------------------------------
 streamlit.stop()
 import snowflake.connector                                                 # imports Snowflake table "fruit_load_list"
 
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")           #<<< this is a Snowflake table, into which people also add manually via Streamlit app
 
-
-
-
-#New section to display fruityvice api response
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit) #<<<< here we set up the end user enters the fruit manually
+streamlit.write('Thanks for adding ', add_my_fruit)                        
 
 #This will not work correctly, but just go with it for now
-#my_cur_execute("insert into fruit_load_list values ('from Streamlit')") #<< this is a trouble allowing me to input data from Streamlit app directly
+#my_cur_execute("insert into fruit_load_list values ('from Streamlit')")           #<< this is a trouble allowing me to input data from Streamlit app directly
 
 
 
