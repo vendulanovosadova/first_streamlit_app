6bdb9385d405c2a6e67f2bf71e538ce9cc1606f0
vendulanovosadova <127227459+vendulanovosadova@users.noreply.github.com>
2023-03-21 08:26:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,11 @@ streamlit.dataframe(fruits_to_show) #<<< this shows the CSV breakdown below the 
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 
 #New section to display fruityvice api response - FIRST ROW WITH MANUAL INPUT
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
-     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')       #<<< ROW WITH MANUAL INPUT, we call in fruit_choice row
+     fruit_choice = streamlit.text_input('What fruit would you like information about?')         #<<< ROW WITH MANUAL INPUT, we call in fruit_choice row
      if not fruit_choice:
-        streamlit.error: ("Please select a fruit to get information.")
+        streamlit.error ("Please select a fruit to get information.")
      else:                                                                                       #<< these are STEPS which will be REPEATING!!!
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())                 #<< take the json version of the response and normalize it  
